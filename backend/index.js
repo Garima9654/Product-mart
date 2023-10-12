023c19ae8983541d87f8d0daecbfa9af30a8fd2e
Garima9654 <76567103+Garima9654@users.noreply.github.com>
2023-10-12 23:18:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; 
 
 //mongodb connection
 mongoose.set("strictQuery", false);
